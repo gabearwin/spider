08fe18501ec08e1a50c4b139b3d3440eb70bb362
gaxiong <gaxiong@ebay.com>
2019-03-31 22:33:14 +0800

    爬取淘宝搜索页商品信息

---
+++ chapter7/config.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+MONGO_URL = 'localhost'
+MONGO_DB = 'taobao'
+MONGO_COLLECTION = 'products'
+
+KEYWORD = 'ipad'
+
+MAX_PAGE = 2
+
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
